2568a596bde97511568ad6a2c56e27d305f27f25
AlphaCentauriRealism <kronixpaypal@gmail.com>
2024-06-11 21:55:03 +0300

    Add files via upload

--- AlphaCentauri.lua
+++ AlphaCentauri.lua
@@ -1,2 +1,2 @@
-getgenv.loadstring(getgenv().http_request({ Url = "https://raw.githubusercontent.com/AlphaCentauriRealism/Orion/main/Andromeda.lua", Method = "GET", Headers = { ['User-Agent'] = 'Roblox/WinInet' } }).Body)()
-getgenv.loadstring(getgenv().http_request({ Url = "https://raw.githubusercontent.com/AlphaCentauriRealism/Orion/main/MilkyWay.lua", Method = "GET", Headers = { ['User-Agent'] = 'Roblox/WinInet' } }).Body)()
+getgenv().loadstring(getgenv().http_request({ Url = "https://raw.githubusercontent.com/AlphaCentauriRealism/Orion/main/Andromeda.lua", Method = "GET", Headers = { ['User-Agent'] = 'Roblox/WinInet' } }).Body)()
+getgenv().loadstring(getgenv().http_request({ Url = "https://raw.githubusercontent.com/AlphaCentauriRealism/Orion/main/MilkyWay.lua", Method = "GET", Headers = { ['User-Agent'] = 'Roblox/WinInet' } }).Body)()
